c16cc5bebea7808aabd760b1316a0f3ef236e76d
GrayJunZi <GrayJunZi@outlook.com>
2024-02-01 10:49:26 +0800

    第十天:带返回值的函数(代码修改)

--- Day010-FunctionsWithOutputs/4-calculator.py
+++ Day010-FunctionsWithOutputs/4-calculator.py
@@ -22,21 +22,39 @@ operators = {
     "*": multiply,
     "/": divide
 }
+
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
 def calculator():
-    num1 = float(input("What's the first number?: "))
-    for symbol in operators:
-        print(symbol)
+    print(logo)
+    num1 = float(input("第一个数字是什么?: "))
+
+    print([symbol for symbol in operators])
 
     should_continue = True
-
     while should_continue:
-        operation_symbol = input("Pick an operation: ")
-        num2 = float(input("What's the next number?: "))
+        operation_symbol = input("选择一个操作: ")
+        num2 = float(input("下一个数字是什么?: "))
         calculation_function = operators[operation_symbol]
         answer = calculation_function(num1,num2)
         print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-        if input(f"Type 'y' to continue calcuting with {answer}, or type 'n' to start a new calculation: ") == 'y':
+        if input(f"输入 'y' 继续计算 {answer}, 或者输入 'n' 开始一个新的计算: ") == 'y':
             num1 = answer
         else:
             should_continue = False
